0722815679ca494f686b21b1c97e000f56c0dc3f
Nagavenkat Adurthi <n.adurthi@gmail.com>
2017-05-13 00:35:00 -0500

    dataframe plotter

--- trade_analytics/featureapp/tornadosebsocket_A.py
+++ trade_analytics/featureapp/tornadosebsocket_A.py
@@ -24,6 +24,7 @@ try:
     from cStringIO import StringIO
 except:
     from StringIO import StringIO
+import multiprocessing as mp
 
 import time
 # Import 3rd-party modules.
@@ -35,8 +36,17 @@ import numpy
 from io import BytesIO
 import json
 import webbrowser
-ip=9009
-def RUN(df):
+
+import zmq
+import pickle as pkl
+import zlib
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+
+def RUN(df,ip):
     server=None
     class IndexHandler(web.RequestHandler):
         def get(self):
@@ -47,7 +57,11 @@ def RUN(df):
             print 'new connection'
         def on_close(self):
             print 'connection closed'
-     
+            ioloop = tornado.ioloop.IOLoop.current().instance()
+            # ioloop = tornado.ioloop.IOLoop.instance()
+            ioloop.add_callback(server.stop)
+            ioloop.add_callback(ioloop.stop)
+
         def check_origin(self, origin):
             return True
 
@@ -57,8 +71,10 @@ def RUN(df):
             if 'cmnd' in message:
                 if message['cmnd']=='quit':
                     # self.close()
-                    ioloop = tornado.ioloop.IOLoop.instance()
-                    ioloop.add_callback(ioloop.stop)
+                    # tornado.ioloop.IOLoop.make_current()
+                    
+                    # time.sleep(1)
+                    # ioloop.add_callback(ioloop.close)
                     self.close()
             try:
                 fig,ax=plt.subplots(1,1,figsize=(10,7))
@@ -79,22 +95,66 @@ def RUN(df):
         (r'/ws', SocketHandler),
     ])
     server=httpserver.HTTPServer(app)
-
+    
     server.listen(ip)
     webbrowser.open('http://localhost:%s'%str(ip),new=2)
-    tornado.ioloop.IOLoop.instance().start()
+    # tornado.ioloop.IOLoop.clear_instance()
+    # tornado.ioloop.IOLoop.instance().install()
+    tornado.ioloop.IOLoop.current().instance().start()
+        
+
     server.stop()
     # ioloop = tornado.ioloop.IOLoop.instance()
+    # ioloop.stop()
+    # ioloop.close()
     # ioloop.add_callback(ioloop.close)
 
-
+    print "stopped"
 
 if __name__ == '__main__':
-    df=pd.DataFrame({'A':[1,2,3,4],'B':[8,7,6,5]})
-    thrd=threading.Thread(target=RUN,args=(df.copy(),))
-    thrd.start()
-    thrd.join()
+    i=0
+    P=[]
+    while (1):
+        try:
+            message = socket.recv(flags=zmq.NOBLOCK)
+            z=zlib.decompress(message)
+            df=pkl.loads(z)
+            print "---------%s----------------"%str(i) 
+            ip=3345+i
+            p=mp.Process(target=RUN,args=(df,ip))   
+            p.start()
+            P.append([p,time.time()])
+            i=i+1
+
+            #  Send reply back to client
+            socket.send("received dataframe")
+
+        except zmq.Again as e:
+            print "No messages"
+        
+        for p in P:
+            p[0].join(0.2)
+            if time.time()-p[1]>100:
+                p[0].terminate()
+
+        time.sleep(0.5)
+
+    # # thrd1=threading.Thread(target=RUN,args=(df.copy(),ip))
+    # # thrd1.start()
+    
+    # print "---------2----------------" 
+    # ip=ip+1
+    # p2=mp.Process(target=RUN,args=(df.copy(),ip))    
+    # p2.start()
+    # # thrd2=threading.Thread(target=RUN,args=(df.copy(),ip))
+    # # thrd2.start()
+
+    # p1.join()
+    # p2.join()
+
+    # thrd1.join()
+    # thrd2.join()
     # print "Your web server will self destruct in 2 minutes"
     # time.sleep(100)
     # stopTornado()
-    thrd.join()
+    # thrd.join()
