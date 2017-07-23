corrected the normalization

trade_analytics/dataapp/stockdata_transformers.py
@@ -533,14 +533,20 @@ def StandardizeData_Close01Volume01_X30_Y5_flatout(X,Y,Meta):
 
         dfX['Volume']=dfX['Volume']/mxvol
         
-        mx=dfX['Close'].max()
+        
         mn=dfX['Close'].min()
 
+        
+
+        dfX['Close']=(dfX['Close']-mn)
+
+        mx=dfX['Close'].max()
         if mx==0:
             continue
 
-        dfX['Close']=(dfX['Close']-mn)/mx
-        
+        dfX['Close']=dfX['Close']/mx
+
+                    
         XX=np.expand_dims( dfX[FinalXcols].astype(float).values.flatten(order='F')   ,axis=0     )
         YY=np.expand_dims( np.array([int(Ydict[key]) for key in FinalYcols]),axis=0 )
         if Xn is None:

trade_analytics/datascience/ML/MLlibs.py
@@ -29,24 +29,23 @@ def get_train_test_from_RawProcessed(DataId):
 	
 
 	for split in range(5):
+		
+		
+
+		projectid,traindataId=dtsclibs.register_dataset(ParentDataId=DataId,Datatype='Train',tag=str(data.tag)+'_train_'+str(split) , DeleteShards=True)
+		projectid,validdataId=dtsclibs.register_dataset(ParentDataId=DataId,Datatype='Validation',tag=str(data.tag)+'_valid_'+str(split) , DeleteShards=True)
 		s=time.time()
-		s=(s-int(s))* 10000+split
-		N=np.random.seed(s)
+		N=int( (s-int(s))* 10000+split )
 
-		projectid,traindataId=dtsclibs.register_dataset(ParentdataId=DataId,Datatype='Train',tag=str(data.tag)+'_train_'+str(split) )
-		projectid,validdataId=dtsclibs.register_dataset(ParentdataId=DataId,Datatype='Validation',tag=str(data.tag)+'_valid_'+str(split) )
-
-		for shard in dtscmd.DataShard.objects.filter(Data=data) :
-			dtsctks.train_valid_split(shard.id,traindataId,validdataId,N)
+		# for shard in dtscmd.DataShard.objects.filter(Data=data) :
+		dtsctks.train_valid_split.delay(traindataId,validdataId,N)
 
 			
 	# create more validation sets
 	for split in range(5,10):
-		s=time.time()
-		s=(s-int(s))* 10000+split
-		N=np.random.seed(s)
-
-		projectid,validdataId=dtsclibs.register_dataset(ParentdataId=DataId,Datatype='Validation',tag=str(data.tag)+'_valid_'+str(split) )
 		
-		for shard in dtscmd.DataShard.objects.filter(Data=data) :
-			dtsctks.train_valid_split(shard.id,None,validdataId,N)
+		projectid,validdataId=dtsclibs.register_dataset(ParentDataId=DataId,Datatype='Validation',tag=str(data.tag)+'_valid_'+str(split) )
+		s=time.time()
+		N=int( (s-int(s))* 10000+split )
+
+		dtsctks.train_valid_split.delay(None,validdataId,N)