e466c752b88724308b64a8cba5703ba3c9b0f736
Nagavenkat Adurthi <n.adurthi@gmail.com>
2017-09-08 14:10:10 -0500

    f

--- trade_analytics/databases/backupdb.sh
+++ trade_analytics/databases/backupdb.sh
@@ -1,8 +1,7 @@
 currdir=`pwd`;
 echo $currdir;
-
+pg_dump -U nagavenkat -w -h 127.0.0.1 trade_analytics > trade_analytics_09-07-17.backup
 pg_dump trade_analytics > $currdir/backup/trade_analytics.backup
 pg_dump featuredata > $currdir/backup/featuredata.backup
 pg_dump querydata > $currdir/backup/querydata.backup
 pg_dump stockpricedata > $currdir/backup/stockpricedata.backup
-
